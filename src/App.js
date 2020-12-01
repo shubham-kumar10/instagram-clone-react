c9702506b67cea7e6a16ffb93c9e55a33560f1b3
shubham.snu@gmail.com <shubham.snu@gmail.com>
2020-12-01 18:06:51 +0530

    App init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app--header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          className="app--header-logo"
+          alt="logo"
+        />
       </header>
     </div>
   );
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div className="post">
+      <div className="post--header"></div>
+    </div>
+  );
+}
+
+export default Post;
